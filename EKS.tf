89806d7800b99c79a595beac96a93bee24e0fb44
Noob-Saibot-227410 <devops.davi@gmail.com>
2024-05-18 01:49:19 -0300

    incrementação de pipeline e uso do EKS

---
+++ EKS.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_ami" "latest_eks_worker" {
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-*"]
+  }
+
+  most_recent = true
+  owners      = ["602401143452"]
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.20"
+  vpc_id          = var.vpc_id
+  subnets         = var.subnets
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 10
+      min_capacity     = 1
+      instance_type    = "m4.large"
+      ami_id           = data.aws_ami.latest_eks_worker.id
+      additional_tags  = {
+        Environment = "test"
+        Name        = "audsat-eks-worker-node"
+      }
+    }
+  }
+}
